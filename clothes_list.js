c3e326043919f3825b797f2cdd084c3126534ca4
chriskarap <karapetyanchrsitina@gmail.com>
2017-12-15 14:21:23 +0400

    multyScenes

---
+++ clothes_list.js
@@ -0,0 +1,44 @@
+const clothes = [
+    {
+        url: './models/female_objs/blue_jeans/',
+        obj: 'Blue_Jeans.obj',
+        mtl: 'Blue_Jeans.mtl',
+        type: 'trousers',
+        visible: true,
+    },
+    {
+        url: './models/female_objs/dark_blue_jeans/',
+        obj: 'dark_blue_jeans.obj',
+        mtl: 'dark_blue_jeans.mtl',
+        type: 'trousers',
+        visible: false,
+    },
+    {
+        url: './models/female_objs/shorts/',
+        obj: 'shortik.obj',
+        mtl: 'shortik.mtl',
+        type: 'shorts',
+        visible: false,
+    },
+    {
+        url: './models/female_objs/dress/',
+        obj: 'dress.obj',
+        mtl: 'dress.mtl',
+        type: 'dress',
+        visible: false,
+    },
+    {
+        url: './models/female_objs/jacket_blue/',
+        obj: 'jacket_blue.obj',
+        mtl: 'jacket_blue.mtl',
+        type: 'jacket',
+        visible: true,
+    },
+    {
+        url: './models/female_objs/jacket_black/',
+        obj: 'jacket_black.obj',
+        mtl: 'jacket_black.mtl',
+        type: 'jacket',
+        visible: false,
+    }
+];
