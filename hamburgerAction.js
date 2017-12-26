11787a16285259c8757520e4fa32b183cd5c549c
chriskarap <karapetyanchrsitina@gmail.com>
2017-12-26 16:48:22 +0400

    fix responsive bug

--- hamburgerAction.js
+++ hamburgerAction.js
@@ -8,11 +8,9 @@ const hamburgerAction = () => {
     const filtersHamburger = document.getElementById('filters-hamburger');
 
     filtersHamburger.addEventListener('mouseup', () => {
-        // if (window.innerWidth <= 991) {
-            (marginFilters === 0) ? marginFilters = -filtersBody.offsetWidth : marginFilters = 0;
-            filters.style.marginLeft = marginFilters + 'px';
-            (+filters.style.zIndex == 13) ? filters.style.zIndex = '12' : filters.style.zIndex = '13';
-        // }
+        (marginFilters === 0) ? marginFilters = -filtersBody.offsetWidth : marginFilters = 0;
+        filters.style.marginLeft = marginFilters + 'px';
+        (+filters.style.zIndex == 13) ? filters.style.zIndex = '12' : filters.style.zIndex = '13';
     });
 
     let marginClothes = 0;
@@ -23,18 +21,22 @@ const hamburgerAction = () => {
     const clothesHamburger = document.getElementById('clothes-hamburger');
 
     clothesHamburger.addEventListener('mouseup', () => {
-        // if (window.innerWidth <= 991) {
-            (marginClothes === 0) ? marginClothes = clothesBody.offsetWidth : marginClothes = 0;
-            clothes.style.marginLeft = marginClothes + 'px';
-            (+clothes.style.zIndex == 13) ? clothes.style.zIndex = '12' : clothes.style.zIndex = '13';
-        // }
+        (marginClothes === 0) ? marginClothes = clothesBody.offsetWidth : marginClothes = 0;
+        clothes.style.marginLeft = marginClothes + 'px';
+        (+clothes.style.zIndex == 13) ? clothes.style.zIndex = '12' : clothes.style.zIndex = '13';
     });
 
-    // window.addEventListener('resize', ()=>{
-    //     if (window.innerWidth > 991) {
-    //         filters.style.marginLeft = 0 + 'px';
-    //         clothes.style.marginLeft = 0 + 'px';
-    //     }
-    // });
+    window.addEventListener('resize', () => {
+        if (window.innerWidth <= 991) {
+            if (marginFilters !== 0)
+                filters.style.marginLeft = -filtersBody.offsetWidth + 'px';
+
+            if (marginClothes !== 0)
+                clothes.style.marginLeft = clothesBody.offsetWidth + 'px';
+        } else {
+            filters.style.marginLeft = 0 + 'px';
+            clothes.style.marginLeft = 0 + 'px';
+        }
+    });
 
 };
